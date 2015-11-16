3ad952c447641e0382d48cb09c94366d45dee7ee
SaleStojkovic <pudingicokolada@gmail.com>
2015-11-16 22:09:44 +0100

    Simple changes Parser.java
    I just merged two functions.

--- Parser.java
+++ Parser.java
@@ -7,35 +7,41 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
+  
   public synchronized void setFile(File f) {
     file = f;
   }
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
+   
+  public String getContent(int val) throws IOException {
     FileInputStream i = new FileInputStream(file);
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    //user wants without unicode
+    if (val == 1) {
+      while ((data = i.read()) > 0) {
+        if (data < 0x80) {
+          output += (char) data;
+          return output;
+        }
+      }
+    } 
+    //user wants with unicoded
+    else {
+        while ((data = i.read()) > 0) {
+          output += (char) data;
+        }
       }
     }
+    
     return output;
   }
+  
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
+    for (int i = 0; i < content.length(); i++) {
       o.write(content.charAt(i));
     }
   }
